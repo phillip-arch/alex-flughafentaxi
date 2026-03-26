56fafcd0a76f7c364b353f00f7fdcb478424fddf
phillip-arch <phillip@itspot.us>
2026-03-27 00:37:13 +0100

    Remove extra app header label

--- components/account/AccountAppHeader.tsx
+++ components/account/AccountAppHeader.tsx
@@ -7,7 +7,7 @@ export default function AccountAppHeader() {
     <header className="border-b border-white/10 bg-[#050505] text-white">
       <div className="app-container">
         <div className="flex h-[66px] items-center justify-between gap-4 lg:h-[72px]">
-          <div className="flex items-center gap-3">
+          <div className="flex items-center">
             <span className="relative block h-11 w-[120px] overflow-hidden lg:h-12 lg:w-[220px]">
               <Image
                 src="https://web-site.website/images/aflogo.jpg"
@@ -17,12 +17,6 @@ export default function AccountAppHeader() {
                 className="object-contain object-left"
               />
             </span>
-            <div className="min-w-0">
-              <p className="text-[0.72rem] font-semibold uppercase tracking-[0.16em] text-[#5ea2ff]">
-                Kundenkonto
-              </p>
-              <p className="text-[1rem] font-semibold tracking-[-0.03em] text-white">App</p>
-            </div>
           </div>
 
           <form action={logout}>
